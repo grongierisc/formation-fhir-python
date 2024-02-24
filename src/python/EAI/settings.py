a585c0e679c8d8bdd1f60035a335a4c458df8755
grongierisc <guillaume.rongier@intersystems.com>
2024-02-24 16:07:51 +0100

    Add PRODUCTIONS configuration to settings.py

--- src/python/EAI/settings.py
+++ src/python/EAI/settings.py
@@ -7,3 +7,84 @@ from bp import MyBusinessProcess
 CLASSES = {
     'Python.EAI.bp.MyBusinessProcess' : MyBusinessProcess
 }
+
+PRODUCTIONS = [
+    {
+    "EAIPKG.FoundationProduction": {
+        "@Name": "EAIPKG.FoundationProduction",
+        "@LogGeneralTraceEvents": "false",
+        "Description": "",
+        "ActorPoolSize": "1",
+        "Item": [
+            {
+                "@Name": "InteropService",
+                "@Category": "",
+                "@ClassName": "HS.FHIRServer.Interop.Service",
+                "@PoolSize": "0",
+                "@Enabled": "true",
+                "@Foreground": "false",
+                "@Comment": "",
+                "@LogTraceEvents": "false",
+                "@Schedule": "",
+                "Setting": [
+                    {
+                        "@Target": "Host",
+                        "@Name": "TargetConfigName",
+                        "#text": "Python.EAI.bp.MyBusinessProcess"
+                    },
+                    {
+                        "@Target": "Host",
+                        "@Name": "TraceOperations",
+                        "#text": "*FULL*"
+                    }
+                ]
+            },
+            {
+                "@Name": "HS.FHIRServer.Interop.HTTPOperation",
+                "@Category": "",
+                "@ClassName": "HS.FHIRServer.Interop.HTTPOperation",
+                "@PoolSize": "1",
+                "@Enabled": "true",
+                "@Foreground": "false",
+                "@Comment": "",
+                "@LogTraceEvents": "false",
+                "@Schedule": "",
+                "Setting": [
+                    {
+                        "@Target": "Host",
+                        "@Name": "ServiceName",
+                        "#text": "fhir"
+                    },
+                    {
+                        "@Target": "Host",
+                        "@Name": "TraceOperations",
+                        "#text": "*FULL*"
+                    }
+                ]
+            },
+            {
+                "@Name": "Python.EAI.bp.MyBusinessProcess",
+                "@Category": "",
+                "@ClassName": "Python.EAI.bp.MyBusinessProcess",
+                "@PoolSize": "1",
+                "@Enabled": "true",
+                "@Foreground": "false",
+                "@Comment": "",
+                "@LogTraceEvents": "false",
+                "@Schedule": ""
+            },
+            {
+                "@Name": "HS.Util.Trace.Operations",
+                "@Category": "",
+                "@ClassName": "HS.Util.Trace.Operations",
+                "@PoolSize": "1",
+                "@Enabled": "true",
+                "@Foreground": "false",
+                "@Comment": "",
+                "@LogTraceEvents": "false",
+                "@Schedule": ""
+            }
+        ]
+    }
+}
+]
