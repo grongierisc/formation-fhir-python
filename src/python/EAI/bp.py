f1bbd3d06cc7d5bb5b825402cf00251c74b86b78
grongierisc <guillaume.rongier@intersystems.com>
2024-02-26 15:52:58 +0100

    Implement check_token and filter_patient_resource methods

--- src/python/EAI/bp.py
+++ src/python/EAI/bp.py
@@ -18,39 +18,10 @@ class MyBusinessProcess(BusinessProcess):
         # pass it to the target
         rsp = self.send_request_sync(self.target, request)
 
-        # Try to get the token from the request
-        token = request.Request.AdditionalInfo.GetAt("USER:OAuthToken") or ""
-
-        # Do something with the response
-        if self.check_token(token):
-            self.log_info('Filtering the response')
-            # Filter the response
-            payload_str = self.quick_stream_to_string(rsp.QuickStreamId)
-
-            # if the payload is empty, return the response
-            if payload_str == '':
-                return rsp
-
-            filtered_payload_string = self.filter_resources(payload_str)
-
-            # write the json string to a quick stream
-            quick_stream = self.string_to_quick_stream(filtered_payload_string)
-
-            # return the response
-            rsp.QuickStreamId = quick_stream._Id()
-
         return rsp
     
     def check_token(self, token:str) -> bool:
-
-        # decode the token
-        decoded_token= jwt.decode(token, options={"verify_signature": False})
-
-        # check if the token is valid
-        if 'VIP' in decoded_token['scope']:
-            return True
-        else:
-            return False
+        raise NotImplementedError('You must implement the check_token method')
 
     def quick_stream_to_string(self, quick_stream_id) -> str:
         quick_stream = iris.cls('HS.SDA3.QuickStream')._OpenId(quick_stream_id)
@@ -72,38 +43,7 @@ class MyBusinessProcess(BusinessProcess):
         return quick_stream
 
     def filter_patient_resource(self, patient_str:str) -> str:
-        # filter the patient
-        p = patient.Patient(**json.loads(patient_str))
-        # remove the name
-        p.name = []
-        # remove the address
-        p.address = []
-        # remove the telecom
-        p.telecom = []
-        # remove the birthdate
-        p.birthDate = None
-
-        return p.json()
+        raise NotImplementedError('You must implement the filter_patient_resource method')
 
     def filter_resources(self, resource_str:str) -> str:
-        # parse the payload
-        payload_dict = json.loads(resource_str)
-
-        # what is the resource type?
-        resource_type = payload_dict['resourceType'] if 'resourceType' in payload_dict else 'None'
-        self.log_info('Resource type: ' + resource_type)
-
-        # is it a bundle?
-        if resource_type == 'Bundle':
-            obj = bundle.Bundle(**payload_dict)
-            # filter the bundle
-            for entry in obj.entry:
-                if entry.resource.resource_type == 'Patient':
-                    self.log_info('Filtering a patient')
-                    entry.resource = patient.Patient(**json.loads(self.filter_patient_resource(entry.resource.json())))
-
-        elif resource_type == 'Patient':
-            # filter the patient
-            obj = patient.Patient(**json.loads(self.filter_patient_resource(resource_str)))
-
-        return obj.json()
+        raise NotImplementedError('You must implement the filter_resources method')
