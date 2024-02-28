be9846a01a87764cc490a46a2095d5c78539d951
grongierisc <guillaume.rongier@intersystems.com>
2024-02-28 12:17:50 +0100

    Add CustomOperationHandler to custom.py

--- src/python/custom.py
+++ src/python/custom.py
@@ -1,4 +1,4 @@
-from FhirInteraction import Interaction, Strategy, OAuthInteraction
+from FhirInteraction import Interaction, Strategy, OAuthInteraction, OperationHandler
 
 from google.oauth2 import id_token
 from google.auth.transport import requests
@@ -173,6 +173,37 @@ class CustomInteraction(Interaction):
                 return False
         else:
             return True
+        
+class CustomOperationHandler(OperationHandler):
+    
+        def add_supported_operations(self,map:dict) -> dict:
+            """
+            @API Enumerate the name and url of each Operation supported by this class
+            @Output map : A map of operation names to their corresponding URL.
+            Example:
+            return map.put("restart","http://hl7.org/fhir/OperationDefinition/System-restart")
+            """
+            return map
+    
+        def process_operation(
+            self,
+            operation_name:str,
+            operation_scope:str,
+            body:dict,
+            fhir_service:'iris.HS.FHIRServer.API.Service',
+            fhir_request:'iris.HS.FHIRServer.API.Data.Request',
+            fhir_response:'iris.HS.FHIRServer.API.Data.Response'
+        ) -> 'iris.HS.FHIRServer.API.Data.Response':
+            """
+            @API Process an Operation request.
+            @Input operation_name : The name of the Operation to process.
+            @Input operation_scope : The scope of the Operation to process.
+            @Input fhir_service : The FHIR Service object.
+            @Input fhir_request : The FHIR Request object.
+            @Input fhir_response : The FHIR Response object.
+            @Output : The FHIR Response object.
+            """
+            return fhir_response
 
 def set_capability_statement():
     from FhirInteraction import Utils
