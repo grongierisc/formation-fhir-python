79a68943baea3fa21bad5da6d2a8afe752819fe5
grongierisc <guillaume.rongier@intersystems.com>
2025-06-10 15:57:24 +0200

    Refactor BusinessProcess import and enhance FHIR request handling with token retrieval

--- src/python/EAI/bp.py
+++ src/python/EAI/bp.py
@@ -1,4 +1,4 @@
-from grongier.pex import BusinessProcess
+from iop import BusinessProcess
 import iris
 import jwt
 import json
@@ -10,16 +10,35 @@ class MyBusinessProcess(BusinessProcess):
         if not hasattr(self, 'target'):
             self.target = 'HS.FHIRServer.Interop.HTTPOperation'
             return
+        
+    def to_request(self, request):
+        self.log_info('Received a request')
 
     def on_fhir_request(self, request:'iris.HS.FHIRServer.Interop.Request'):
         # Do something with the request
         self.log_info('Received a FHIR request')
+
+        self.logger.debug(f'Request: {request}')
+
+        token_id = request.Request.AdditionalInfo.GetAt("USER:TokenId")
+
+        token = self.get_token_string(token_id)
 
         # pass it to the target
         rsp = self.send_request_sync(self.target, request)
 
         return rsp
     
+    def get_token_string(self, token_id:str) -> str:
+        """
+        Returns the token string from the JWT token.
+        """
+        ## ##class(HS.HC.Util.InfoCache).GetTokenInfoItem(tokenCacheId, "token_string")
+        token_info = iris.cls('HS.HC.Util.InfoCache').GetTokenInfoItem(token_id, 'token_string')
+        if not token_info:
+            raise ValueError(f'Token with ID {token_id} not found in the cache.')
+        return token_info
+
     def check_token(self, token:str) -> bool:
         raise NotImplementedError('You must implement the check_token method')
 
